c997606a9e783252295a5669be74ddd7348de426
Michael Flanders <mflanders06@gmail.com>
2021-03-27 16:39:45 -0600

    Finished

--- server/getProducts.js
+++ server/getProducts.js
@@ -1,7 +1,18 @@
 const products = require('../products.json');
 
 const getProducts =  (req, res, next) =>{
-    res.status(200).json(products)
+    if (req.query.price){
+        const pricedProducts = products.filter(( value ) =>  value.price >= parseInt(req.query.price));
+        if (pricedProducts.length > 0){
+            res.status(200).json(pricedProducts);
+        }
+        else {
+            res.status(500).json('No products priced that high');
+        }
+    }
+    else {
+        res.status(200).json(products)
+    }
 } 
 
 module.exports = getProducts;
