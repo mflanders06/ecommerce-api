7f53a4b1fc6598b270d1898e3c422d29a582971e
Michael Flanders <mflanders06@gmail.com>
2021-03-27 16:19:48 -0600

    getProduct is working

--- server/getProduct.js
+++ server/getProduct.js
@@ -1,14 +1,16 @@
 const products = require('../products.json');
 
-console.log('I got here');
+const getProduct =  (req, res) =>{
+    const  { id } = req.params; //API passed in parameter (as string)
+    const idNum = parseInt(id); //parameter cast as number
+    const product = products.filter(( value ) => value.id === idNum);
+    
+    if (product.length > 0){
+        res.status(200).json(product);
+    }
+    else {res.status(500).json('Item not in the list')
 
-const getProduct =  (req, res, next) =>{
-    const  { id } = req.params;
-    const product = products.filter(( val ) => {
-        val.id = id;
-    })
-    console.log(product);
-    res.status(200).json(product)
+    }
 } 
 
 module.exports = getProduct;
